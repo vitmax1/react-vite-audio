76d91ff66b7ee70680646b6f9cd4ee35a5c95a7f
vitmax1 <2california@mail.ru>
2025-09-07 00:38:10 +0300

    css-break-w-full-2

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
         >
             <div className="absolute inset-0 bg-black opacity-55"></div>
             <div className="flex justify-center items-center h-screen">
-                <div className="w-full md:w-auto z-999 md:p-4">
+                <div className="w-full sm:w-auto z-999 md:p-4">
                     {/* <h1 className="text-3xl font-bold mb-6 text-center">
                     🎶 Слушай RAP
                 </h1> */}
